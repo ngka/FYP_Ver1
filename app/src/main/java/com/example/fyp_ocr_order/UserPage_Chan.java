04d2be7b62637d895c8826f76dfcf13725d707b4
DESKTOP-TKB7LV1\evang <210298728@stu.vtc.edu.hk>
2023-11-10 00:28:27 +0800

    add activity_user_chan.xml add UserPage_Chan.java 我(Chan)額外加了User的主頁面
    原本：
    activity_userhome_ng.xml
    PublicuserPage_Ng.java

---
+++ app/src/main/java/com/example/fyp_ocr_order/UserPage_Chan.java
@@ -0,0 +1,54 @@
+package com.example.fyp_ocr_order;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserPage_Chan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userhome_chan);
+        setTitle("Home Page");
+
+
+        Button ocr = findViewById(R.id.ocr);
+        Button cnn = findViewById(R.id.cnn);
+        Button feedback = findViewById(R.id.feedback);
+        Button add_data = findViewById(R.id.add_data);
+
+        add_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserPage_Chan.this, User_SendProblem_Ng.class);
+                startActivity(intent);
+            }
+        });
+
+        feedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserPage_Chan.this, Feedback_Ng.class);
+                startActivity(intent);
+            }
+        });
+        ocr.setOnClickListener(view -> {
+            Intent intent = new Intent(UserPage_Chan.this, OCR_Ng.class);
+            startActivity(intent);
+        });
+        cnn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserPage_Chan.this, CNN_Ng.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+}
